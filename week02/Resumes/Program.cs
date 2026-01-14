7be0e0921d0aa39036b3aabe548437b46706e934
DerekNielsenCSWB110 <dnielsen@byupathway.edu>
2026-01-14 13:36:38 -0800

    Update job details and resume name in Program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._jobTitle = "Computer Programmer";
+        job1._company = "Broski Studios";
+        job1._startYear = 2023;
+        job1._endYear = 2025;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
+        job2._jobTitle = "Website Developer";
+        job2._company = "Tech Solutions";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Derek Nielsen";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
