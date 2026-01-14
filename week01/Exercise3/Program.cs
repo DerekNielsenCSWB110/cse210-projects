0927d889dd295340f8a4710c139443a277bc3f6f
DerekNielsenCSWB110 <dnielsen@byupathway.edu>
2026-01-13 17:44:51 -0800

    Implement exercises for user input, grade calculation, number guessing, and statistics

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random ();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+
+            while (guess != magicNumber)
+            {
+
+                // Always make sure to add a space at the end so the imput isn't combined with the prompt
+                Console.Write("Take a guess ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Yep");
+                    Console.WriteLine($"You got it in {guessCount} tries");
+                }
+            }
+
+            Console.Write("Want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
