751350eb56c66c3fb7b943ef340e486152d0bf60
DerekNielsenCSWB110 <dnielsen@byupathway.edu>
2026-01-14 13:38:44 -0800

    Swap job details for job1 and job2 in Program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,17 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        
         Job job1 = new Job();
-        job1._jobTitle = "Computer Programmer";
-        job1._company = "Broski Studios";
-        job1._startYear = 2023;
-        job1._endYear = 2025;
-
+        job1._jobTitle = "Website Developer";
+        job1._company = "Tech Solutions";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+        
+        
         Job job2 = new Job();
-        job2._jobTitle = "Website Developer";
-        job2._company = "Tech Solutions";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._jobTitle = "Computer Programmer";
+        job2._company = "Broski Studios";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
 
         Resume myResume = new Resume();
         myResume._name = "Derek Nielsen";
